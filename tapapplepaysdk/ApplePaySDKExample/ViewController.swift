23ea877fa63083a2e8b99986b43605a67a28e5bc
O Rabie <o.rabie@tap.company>
2022-12-08 17:27:56 +0200

    Added Init api models response/request

---
+++ tapapplepaysdk/ApplePaySDKExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ApplePaySDKExample
+//
+//  Created by Osama Rabie on 08/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ tapapplepaysdk/tapapplepaysdk/tapapplepaysdk.h
@@ -0,0 +1,18 @@
+//
+//  tapapplepaysdk.h
+//  tapapplepaysdk
+//
+//  Created by Osama Rabie on 08/12/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for tapapplepaysdk.
+FOUNDATION_EXPORT double tapapplepaysdkVersionNumber;
+
+//! Project version string for tapapplepaysdk.
+FOUNDATION_EXPORT const unsigned char tapapplepaysdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <tapapplepaysdk/PublicHeader.h>
+
+
